89ca091953d3bff6302f1d11e9d21afd57084a75
Victoria_Moraes <victoria.candido.mm@gmail.com>
2024-01-17 17:52:21 -0300

    adicionando componentes footer/navbar e impl rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-
-
-import './App.css'
-import Home from './pages/home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navBar/NavBar';
+import Home from './pages/home/Home';
 
 function App() {
-
-
   return (
-
     <>
-
-      <Home />
-
+    <BrowserRouter>
+      <Navbar />
+      <div className='min-h-[80vh]'>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
+      </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
     </>
-
-  )
+  );
 }
-
-export default App
+export default App;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,41 +1,34 @@
-function Home() {
+import homeLogo from '../../assets/home.png'
 
+function Home() {
     return (
         <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>Seja Bem-Vinde!</h2>
+                        <p className='text-xl'>Expresse aqui seus pensamentos e opiniões</p>
 
-                <div>
-                    <div style={{
-                        width: "80vw",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center"
-                    }}>
-
-                        <h2> Seja Bem Vindo!</h2>
-                        <p>Expresse aqui os seus pensamentos e opiniões</p>
+                        <div id="Botões" className="flex justify-around gap-4 w-1/2 text-center">
+                            <div className="rounded text-white border-white border-solid border-2 px-4 py-2 flex-1">
+                                Nova Postagem
+                            </div>
+                            <div className="rounded text-black border-white bg-white border-solid border-2 px-4 py-2 flex-1">
+                                Ver Postagens
+                            </div>
+                        </div>
                     </div>
-                    <div
-                        style={{
-                            width: "80vw",
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center"
-                        }}>
-                        <img src="https://i.imgur.com/VpwApCU.png"
-                            alt="Ilustração Home"
-                            width="400px" />
 
+                    <div className="flex justify-center ">
+                        <img src={homeLogo} alt="Imagem home" className="w-2/3" />
 
                     </div>
                 </div>
             </div>
+
         </>
     );
 }
 
-export default Home
+export default Home;
+
