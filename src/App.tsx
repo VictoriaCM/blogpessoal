f1c4f477f033745e73dd27385f83b94b41c4ade4
Victoria_Moraes <victoria.candido.mm@gmail.com>
2024-01-18 12:35:23 -0300

    Adicionando e alterando Models, Contexts, Services e pages login/cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Footer from './components/footer/Footer';
+
 import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Navbar />
-      <div className='min-h-[80vh]'>
-      <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/home" element={<Home/>}/>
-      </Routes>
-      </div>
-      <Footer />
-    </BrowserRouter>
+   
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+  
     </>
   );
 }
