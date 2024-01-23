0b732ce9526cd88135425e47a91043631a16a051
Victoria_Moraes <victoria.candido.mm@gmail.com>
2024-01-22 23:43:55 -0300

    componentes de tema
    adicionando layout e componentes lógicos do componente tema

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -4,32 +4,36 @@ import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
 
-  const navigate = useNavigate()
+    const navigate = useNavigate();
 
-  const { handleLogout } = useContext(AuthContext)
+    const { handleLogout} = useContext(AuthContext)
 
-  function logout() {
-    handleLogout()
-    alert("O usuário foi desconectado com sucesso!")
-    navigate("/login")
-  }
+    function logout(){
 
-  return (
-    <>
-      <div className='fixed top-0 w-full bg-indigo-950 text-white flex justify-center py-4 border-b border-white'>
-        <div className="container flex justify-between text-lg">
-          <Link to="/home" className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
-          <div className='flex gap-4'>
-            <div className='hover:underline'>Postagens</div>
-            <div className='hover:underline'>Temas</div>
-            <div className='hover:underline'>Perfil</div>
-            <Link to="/cadastro" className='hover:underline'>Cadastrar Usuário</Link>
-            <div className='hover:underline' onClick={logout}>Sair</div>
-          </div>
-        </div>
-      </div >
-    </>
-  )
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/login')
+    }
+
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
+            
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        Postagens
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        Perfil
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
