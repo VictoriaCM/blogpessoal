88605ec8c659799c7ec1abdbd1504d95f71e4046
Victoria_Moraes <victoria.candido.mm@gmail.com>
2024-01-29 11:39:13 -0300

    alterando paleta

--- src/components/postagens/cardPostagem/CardPostagem.tsx
+++ src/components/postagens/cardPostagem/CardPostagem.tsx
@@ -7,9 +7,9 @@ interface CardPostagemProps {
 
 function CardPostagem({post}: CardPostagemProps) {
   return (
-    <div className='border-slate-900 border flex flex-col rounded overflow-hidden justify-between'>
+    <div className='border-slate-900 border flex flex-col rounded-lg overflow-hidden justify-between'>
       <div>
-        <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
+        <div className="flex w-full bg-slate-700 py-2 px-4 items-center gap-4">
           <img src={post.usuario?.foto} className='h-12 rounded-full' alt="" />
           <h3 className='text-lg font-bold text-center uppercase '>{post.usuario?.nome}</h3>
         </div>
@@ -24,10 +24,10 @@ function CardPostagem({post}: CardPostagemProps) {
         </div>
       </div>
       <div className="flex">
-      <Link to={`/editarPostagem/${post.id}`} className='w-full text-white bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2'>
+      <Link to={`/editarPostagem/${post.id}`} className='w-full text-white bg-indigo-900 hover:bg-indigo-700 flex items-center justify-center py-2'>
           <button>Editar</button>
         </Link>
-        <Link to={`/deletarPostagem/${post.id}`} className='text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
+        <Link to={`/deletarPostagem/${post.id}`} className='text-white bg-red-800 hover:bg-red-700 w-full flex items-center justify-center'>
           <button>Deletar</button>
         </Link>
       </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -13,7 +13,7 @@ function Home() {
   
               <div className="flex justify-around gap-4">
               <ModalPostagem />
-              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+              <button className='rounded bg-white text-slate-800 py-2 px-4'>Ver postagens</button>
             </div>
             </div>
   
